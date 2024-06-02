ccf867b1779d0771c08a2e00fffa9569ee8349c6
Jan Ufniarz <jan.ufniarz@email.com>
2024-06-02 17:17:51 +0200

    split screen and value

--- lib/main.dart
+++ lib/main.dart
@@ -2,69 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const SplitApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SensorDataScreen(),
-    );
-  }
-}
+class SplitApp extends StatelessWidget {
+  const SplitApp({super.key});
 
-class SensorDataScreen extends StatefulWidget {
   @override
-  _SensorDataScreenState createState() => _SensorDataScreenState();
-}
-
-class _SensorDataScreenState extends State<SensorDataScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sensor Data'),
-      ),
-      body: Center(
-        child: StreamBuilder<AccelerometerEvent>(
-          stream: accelerometerEvents,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator(); // Wskaźnik ładowania, gdy strumień czeka na dane
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}'); // Wyświetlenie błędu, jeśli wystąpił
-            } else if (snapshot.hasData) {
-              // Wyświetlenie danych, gdy są dostępne
-              final event = snapshot.data!;
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Accelerometer Data:',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'X: ${event.x.toStringAsFixed(2)}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    'Y: ${event.y.toStringAsFixed(2)}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    'Z: ${event.z.toStringAsFixed(2)}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
+  Widget build(BuildContext context) => MaterialApp(
+    home: Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: List.generate(2, (index) => Expanded(
+              child: Container(
+                color: [Colors.cyan, Colors.red][index],
+              ),
+            ))
+          ),
+          StreamBuilder<AccelerometerEvent>(
+            stream: accelerometerEventStream(),
+            builder: (context, snapshot) {
+              return Center(
+                child: Text("value: ${snapshot.data!.y.toStringAsFixed(2)}"),
               );
-            } else {
-              return Text('No data'); // Wyświetlenie komunikatu, gdy brak danych
             }
-          },
-        ),
+          )
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
